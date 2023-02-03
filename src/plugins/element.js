5fbbf637ad3d50f3fe6a4afcf344a644404136b6
JiaYangLiao <whileyy@163.com>
2023-02-03 10:50:32 +0800

    no message

---
+++ src/plugins/element.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import {
+    Button,
+    Container,
+    Aside,
+    MenuItem,
+    Menu,
+    Header,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    Table,
+    TableColumn,
+    MenuItemGroup,
+    Submenu,
+    Main
+} from 'element-ui'
+
+Vue.use(Button).use(Container).use(Aside).use(MenuItem).use(Menu).use(Header).use(Dropdown).use(DropdownMenu).use(DropdownItem).use(Table).use(TableColumn).use(MenuItemGroup).use(Submenu).use(Main)
